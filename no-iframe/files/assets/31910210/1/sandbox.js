f5b5ec5af3b50faf2dcd4e891f75a9e8834dec45
Steven Yau <social@stevenyau.co.uk>
2020-06-21 15:25:17 +0100

    Removed extra JS CSS code from test project

--- no-iframe/files/assets/31910210/1/sandbox.js
+++ no-iframe/files/assets/31910210/1/sandbox.js
@@ -7,12 +7,6 @@ Sandbox.attributes.add('cameraEntity', {type: 'entity'});
 
 // initialize code called once per entity
 Sandbox.prototype.initialize = function() {
-    this.app.once('postrender', function() {
-        document.getElementById('application-canvas').style.position = 'fixed';
-        document.body.style.minHeight = '-webkit-fill-available';
-        document.documentElement.style.height = '-webkit-fill-available';
-    });
-    
     this.entity.element.on('touchstart', function(e) {
         this.app.graphicsDevice.fullscreen = true;
     }, this);
